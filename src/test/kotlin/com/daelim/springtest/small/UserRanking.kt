41c188032f51ede17cc9d3f47c9a782fdb4c3bd2
KIBO0630 <155514331+KIBO0630@users.noreply.github.com>
2024-05-22 00:21:39 +0900

    플레이타임을 점수화 시키는 알고리즘 생성

---
+++ src/test/kotlin/com/daelim/springtest/small/UserRanking.kt
@@ -0,0 +1,15 @@
+package com.daelim.springtest.small
+
+class UserRanking {
+    companion object {
+        fun calculateScore(playTime: Float): Float {
+            val maxScore = 2000 // 최대 점수
+            val maxPlayTime = 15 // 1분
+            val adjustedPlayTime = playTime.coerceAtMost(maxPlayTime.toFloat()) // 플레이 타임을 최대값인 1분으로 조정
+
+            // 클리어 시간이 1분 안에일 경우 최대 점수 부여
+            val score = maxScore * (1 - adjustedPlayTime / maxPlayTime)
+            return score
+        }
+    }
+}
