4c576debbc00c6954b47bc5d1aa6f47b1e4922f3
KIBO0630 <155514331+KIBO0630@users.noreply.github.com>
2024-05-22 14:13:18 +0900

    TDD_Small_UserRanking

--- src/test/kotlin/com/daelim/springtest/small/UserRanking.kt
+++ src/test/kotlin/com/daelim/springtest/small/UserRanking.kt
@@ -4,7 +4,7 @@ class UserRanking {
     companion object {
         fun calculateScore(playTime: Float): Float {
             val maxScore = 2000 // 최대 점수
-            val maxPlayTime = 15 // 1분
+            val maxPlayTime = 15
             val adjustedPlayTime = playTime.coerceAtMost(maxPlayTime.toFloat()) // 플레이 타임을 최대값인 1분으로 조정
 
             // 클리어 시간이 1분 안에일 경우 최대 점수 부여
